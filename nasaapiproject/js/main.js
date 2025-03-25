1ad177ab86860322d31afc7517ec7c076bf228e3
Francelie <francelie.ovalle@gmail.com>
2025-03-25 17:59:58 -0400

    new project start

--- nasaapiproject/js/main.js
+++ nasaapiproject/js/main.js
@@ -1,1 +1,14 @@
+document.querySelector('button').addEventListener('click', getPictureOfDay)
 
+function getPictureOfDay (){
+    const date = document.querySelector('input').value
+    const url = `https://api.nasa.gov/planetary/apod?api_key=MMlf5VEZo6dpJFnePlW1QFSyUy5suYBmHbocfOzE&date=${date}`
+    fetch (url)
+     .then (resp => resp.json ())
+     .then (data => {
+        document.querySelector('h2').innerText = data.title
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation 
+     } )
+
+    }
