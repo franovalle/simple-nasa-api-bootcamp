7d5d877d0298c14d3f5ef75e82f72dfc6d1fffc4
Francelie <francelie.ovalle@gmail.com>
2025-03-26 08:45:46 -0400

    draft one

--- nasaapiproject/js/main.js
+++ nasaapiproject/js/main.js
@@ -1,14 +1,23 @@
 document.querySelector('button').addEventListener('click', getPictureOfDay)
 
-function getPictureOfDay (){
-    const date = document.querySelector('input').value
-    const url = `https://api.nasa.gov/planetary/apod?api_key=MMlf5VEZo6dpJFnePlW1QFSyUy5suYBmHbocfOzE&date=${date}`
-    fetch (url)
-     .then (resp => resp.json ())
-     .then (data => {
-        document.querySelector('h2').innerText = data.title
-        document.querySelector('img').src = data.hdurl
-        document.querySelector('h3').innerText = data.explanation 
-     } )
+function getPictureOfDay() {
+   const date = document.querySelector('input').value
+   const url = `https://api.nasa.gov/planetary/apod?api_key=MMlf5VEZo6dpJFnePlW1QFSyUy5suYBmHbocfOzE&date=${date}`
+   fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+         document.querySelector('h2').innerText = data.title
+         if (data.media_type === 'image') {
+            document.querySelector('img').src = data.hdurl
+            document.querySelector('iframe').src = ''//data.url.classList.add ('hidden')
+         }
+         else if (data.media_type === 'video')  {
+            document.querySelector('iframe').src = data.url
+            document.querySelector('img').src = '' //data.hdurl.style.display = 'none'
+         }
 
-    }
+         document.querySelector('h3').innerText = data.explanation
+      })
+      
+}
+
